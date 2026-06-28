dd02f5d3ae660d456073fb4e819a2c6e32743c26
Netanel Wallach <netanelwallach@gmail.com>
2026-06-28 12:22:11 +0300

    Dummy component - Ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import './App.css';
-import Spamalot from './components/Spamalot';
-import NavBar from './components/NavBar';
-import Checkout from './components/Checkout';
-import Menu from './components/Menu';
-import Dummy from './components/Dummy';
+import "./App.css";
+import Spamalot from "./components/Spamalot";
+import NavBar from "./components/NavBar";
+import Checkout from "./components/Checkout";
+import Menu from "./components/Menu";
+import Dummy from "./components/Dummy";
 
 export default function App() {
-  
   return (
     <div className="app">
-
       <div className="ex-space">
         <h4 className="ex-title">Exercise 1</h4>
         <div className="exercise" id="ex-1">
           {/* your code here... */}
+          <Dummy />
         </div>
       </div>
 
@@ -31,6 +30,5 @@ export default function App() {
         </div>
       </div>
     </div>
-  )
+  );
 }
-
--- src/components/Dummy.jsx
+++ src/components/Dummy.jsx
@@ -1,4 +1,10 @@
 const Dummy = () => {
-}
+  return (
+    <>
+      <input type="text" placeholder="Type something..." />
+      <button>Click Me</button>
+    </>
+  );
+};
 
-export default Dummy
+export default Dummy;
